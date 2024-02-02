ad43f05bd3cc8e274880dc40d9a2252ff2192ffa
agustfricke <agustfricke@protonmail.com>
2024-02-02 01:52:56 -0300

    edit install script

--- install.sh
+++ install.sh
@@ -4,7 +4,9 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install fzf kitty gimp tmux i3 git build-essential feh lxappearance arc-theme xclip -y 
 git clone https://github.com/agustfricke/dotfiles.git ~/dotfiles
 
-cp -r ~/dotfiles/i3 ~/.config/i3
+cp -r ~/dotfiles/linux-mint/kitty ~/.config
+
+cp -r ~/dotfiles/linux-mint/i3 ~/.config
 
 wget -O ~/FiraCode.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.1/FiraCode.zip 
 unzip ~/FiraCode.zip -d ~/.local/share/fonts 
@@ -27,7 +29,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 sudo chmod a+r /etc/apt/keyrings/docker.gpg 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo $VERSION_CODENAME) stable" | sudo tee /etc/apt/sources.list.d/docker.list 
 sudo rm /etc/apt/sources.list.d/docker.list 
-sudo cp ~/dotfiles/docker.list /etc/apt/sources.list.d/docker.list
+sudo cp ~/dotfiles/linux-mint/docker.list /etc/apt/sources.list.d/docker.list
 sudo apt-get update -y 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y 
 
@@ -40,27 +42,26 @@ mv ~/dotfiles/Wallpapers ~/Pictures
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 cp ~/dotfiles/linux-mint/tmux ~/.config
 
-echo "Instalando y configurando Virt Manager"
-sudo apt install qemu-kvm qemu-system qemu-utils python3 python3-pip libvirt-clients libvirt-daemon-system bridge-utils virtinst libvirt-daemon virt-manager -y > /dev/null 2>&1b
-sudo virsh net-start default > /dev/null 2>&1b
-sudo virsh net-autostart default > /dev/null 2>&1b
-sudo usermod -aG libvirt $USER > /dev/null 2>&1b
-sudo usermod -aG libvirt-qemu $USER > /dev/null 2>&1b
-sudo usermod -aG kvm $USER > /dev/null 2>&1b
-sudo usermod -aG input $USER > /dev/null 2>&1b
-sudo usermod -aG disk $USER > /dev/null 2>&1b
+sudo apt install qemu-kvm qemu-system qemu-utils python3 python3-pip libvirt-clients libvirt-daemon-system bridge-utils virtinst libvirt-daemon virt-manager -y
+sudo virsh net-start default 
+sudo virsh net-autostart default 
+sudo usermod -aG libvirt $USER 
+sudo usermod -aG libvirt-qemu $USER 
+sudo usermod -aG kvm $USER 
+sudo usermod -aG input $USER 
+sudo usermod -aG disk $USER 
 
-git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim /dev/null 2>&1b
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim 
 cp -r ~/dotfiles/nvim ~/.config
 
-sudo apt install zsh -y /dev/null 2>&1b
+sudo apt install zsh -y 
 chsh -s $(which zsh) $USER
 
-wget -qO- https://get.pnpm.io/install.sh | sh - /dev/null 2>&1b
+wget -qO- https://get.pnpm.io/install.sh | sh - 
 
 curl -sLO https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh
 zsh ./install.zsh --branch release-v1
-cp ~/dotfiles/zsh/.zshrc ~/.zshrc
+cp ~/dotfiles/linux-mint/zsh/.zshrc ~/.zshrc
 sudo rm -r ~/.local/share/zap/plugins/zap-prompt
 cp -r ~/dotfiles/linux-mint/zsh/zap-prompt ~/.local/share/zap/plugins
 sudo rm ~/.local/share/zap/plugins/zsh-vi-mode/zsh-vi-mode.plugin.zsh
