3b4b40508d3d3fc07461d5899fac88e98e303115
agustfricke <agustfricke@protonmail.com>
2024-05-18 19:17:19 -0300

    update telescope to 0.1.6

--- nvim/lua/agust/packer.lua
+++ nvim/lua/agust/packer.lua
@@ -4,7 +4,7 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.2',
+	  'nvim-telescope/telescope.nvim', tag = '0.1.6',
 	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
