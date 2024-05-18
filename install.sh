d995f7b71dc9df55a937d74da3a6063bdec20dfd
agustfricke <agustfricke@protonmail.com>
2024-05-18 01:23:38 -0300

    add docker-compose

--- install.sh
+++ install.sh
@@ -33,6 +33,9 @@ sudo cp ~/dotfiles/linux-mint/docker.list /etc/apt/sources.list.d/docker.list
 sudo apt-get update -y 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y 
 
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage  
 chmod +x nvim.appimage
 sudo mv nvim.appimage /usr/local/bin/vim
