e8eb63d92307bd7876cc3831788ac04e7b5ee410
agustfricke <agustfricke@protonmail.com>
2024-05-26 07:49:07 -0300

    add rose pine with no background color

--- nvim/lua/agust/packer.lua
+++ nvim/lua/agust/packer.lua
@@ -18,6 +18,14 @@ return require('packer').startup(function(use)
 
   use "folke/tokyonight.nvim"
 
+  use({
+	  'rose-pine/neovim',
+	  as = 'rose-pine',
+	  config = function()
+		  vim.cmd('colorscheme rose-pine')
+	  end
+  })
+
   use {
     'nvim-lualine/lualine.nvim',
     requires = { 'nvim-tree/nvim-web-devicons', opt = true }
