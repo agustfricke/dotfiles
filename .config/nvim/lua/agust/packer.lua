0129a701e6324af9970a48680b7ea3f143aee776
agustfricke <agustfricke@protonmail.com>
2024-07-12 16:15:08 -0300

    fix packer.lua

--- .config/nvim/lua/agust/packer.lua
+++ .config/nvim/lua/agust/packer.lua
@@ -1,18 +1,6 @@
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
-end
+vim.cmd [[packadd packer.nvim]]
 
-local packer_bootstrap = ensure_packer()
-
-require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
+return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   use {
@@ -33,6 +21,17 @@ require('packer').startup(function(use)
   use {
     'Exafunction/codeium.vim',
   }
+
+
+  --[[
+  use({
+	  'rose-pine/neovim',
+	  as = 'rose-pine',
+	  config = function()
+		  vim.cmd('colorscheme rose-pine')
+	  end
+  })
+]]
 
   use {
     'nvim-lualine/lualine.nvim',
@@ -69,8 +68,4 @@ require('packer').startup(function(use)
             {'L3MON4D3/LuaSnip'},     -- Required
         }
     }
-
-  if packer_bootstrap then
-    require('packer').sync()
-  end
 end)
